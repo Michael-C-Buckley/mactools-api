a645781b9d8c1771f4ec4035130103088bbd5422
Michael Buckley <michaelcbuckley@proton.me>
2024-04-02 20:44:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MacTools API
+
+API Extension of MacTools for a service delivering requests.
+
+MacTools is a MAC-centric library for aiding in network handling and automation.
---
+++ setup.py
@@ -0,0 +1,27 @@
+# MacTools API Setup
+
+# Python Modules
+from setuptools import setup, find_packages
+
+# Local Modules
+from app import __version__
+
+DESCRIPTION = 'MacTools API Server Expansion Module',
+
+with open('README.md', 'r', encoding='utf-8') as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='MacTools',
+    author='Michael Buckley',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type = 'text/markdown',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'mactools',
+        'fastapi',
+    ],
+    keywords=['python','networking','network','mac','oui','ieee','api']
+)
